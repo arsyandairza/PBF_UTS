b55c0992b524c0f72f033c35793fda4c2069def6
Bima Tristan <bimatris13@gmail.com>
2024-05-16 09:25:30 +0700

    fix route error

--- src/pages/ManageDoor.tsx
+++ src/pages/ManageDoor.tsx
@@ -6,25 +6,19 @@ import Navbar1 from "../components/Navbar1";
 const ManageDoor: FunctionComponent = () => {
   const navigate = useNavigate();
 
-  const onManageDoorContainerClick = useCallback(() => {
-    navigate("/manage-door");
-  }, [navigate]);
-
   const onDashboardClick = useCallback(() => {
     navigate("/dashboard");
   }, [navigate]);
 
-  const onFrameContainerClick = useCallback(() => {
+  const onManageUserContainerClick = useCallback(() => {
     navigate("/manage-user");
   }, [navigate]);
-
-
 
   return (
     <div
       className="w-full relative [background:linear-gradient(180deg,#092230,#1c6a96)] h-[1024px] overflow-hidden cursor-pointer text-center text-base text-white font-poppins">
       <Sidebar 
-      onManageUserContainerClick={onFrameContainerClick}
+      onManageUserContainerClick={onManageUserContainerClick}
       onDashboardContainerClick={onDashboardClick}/>
       <Navbar1 />
       <div className="absolute top-[277px] left-[410px] rounded-2xl [background:linear-gradient(25.18deg,_rgba(113,_199,_236,_0.75)_16.96%,_rgba(70,_102,_116,_0.75)_45.33%)] w-80 h-[470px] overflow-hidden">
