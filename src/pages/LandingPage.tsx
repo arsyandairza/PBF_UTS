31f8b29e093c50f348a8e8e7c93f649c20c03818
Bima Tristan <bimatris13@gmail.com>
2024-05-21 21:23:04 +0700

    Fix "Cadenas" text position

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,10 +6,10 @@ const LandingPage: FunctionComponent = () => {
     <div className="w-full relative [background:linear-gradient(180deg,_#092230,_#1c6a96)] overflow-hidden flex flex-col items-center justify-start pt-[77px] px-5 pb-[172px] box-border gap-[20px] leading-[normal] tracking-[normal] text-left text-31xl text-linen font-poppins">
       <div className="w-[870px] flex flex-row items-start justify-center max-w-full">
         <div className="flex flex-row items-start justify-start gap-[17px]">
-          <h1 className="m-0 relative text-inherit font-semibold font-inherit mq450:text-11xl mq750:text-21xl">
+          <h1 className="m-0 relative text-inherit font-semibold font-inherit mq650:text-11xl mq750:text-21xl" style={{ fontSize: '60px', color: 'white'  }}>
             Cadenas
           </h1>
-          <div className="flex flex-col items-start justify-start pt-0.5 px-0 pb-0">
+          <div className="flex flex-col items-start justify-start pt-3 px-0 pb-0">
             <img
               className="w-[66.5px] h-[70px] relative"
               loading="lazy"
